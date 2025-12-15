6e359f4650395a5f82165ec04d76c1239f975ba4
Florian Echtler <floe@butterbrot.org>
2025-12-15 10:34:22 +0100

    fetch & parse works

--- dataFetching/openalex_fetch.py
+++ dataFetching/openalex_fetch.py
@@ -10,29 +10,60 @@ author = sys.argv[1]
 supervisor = sys.argv[2]
 
 res = requests.get("https://api.openalex.org/authors/orcid:"+author)
-print(res,res.text)
+#print(res,res.text)
 author = res.json()
 
 res = requests.get(author["works_api_url"]+"&per_page=200") # request maximum per-page so we don't need to paginate
 works = res.json()
 
+# collect previous positions
 Mobility = []
 for aff in author["affiliations"]:
     position = {
-        "Position": "Master/PhD/PostDoc/AsstProf/AssocProf/FullProf",
+        "Position": "Master/PhD/PostDoc/AsstProf/AssocProf/FullProf", # needs to be manually filled in
         "StartDate": aff["years"][-1],
         "EndDate": aff["years"][0],
-        "Location": "TBD",
+        "Location": "TBD", # can be queried from institution["id"] -> geo.city/country
         "Institute": aff["institution"]["display_name"]
     }
     Mobility.insert(0,position)
+
+Works = []
+for work in works["results"]:
+
+    with_advisor = "No"
+    author_list = ""
+
+    for coauthor in work["authorships"]:
+        if supervisor in str(coauthor["author"]["orcid"]):
+            with_advisor = "Yes"
+        author_list = author_list+", "+str(coauthor["author"]["display_name"])
+
+    publication = {
+        "Author": author["display_name"],
+        "Title": work["title"],
+        "Type": "Paper", # TBD
+        "Category": work["primary_location"]["raw_type"],
+        "JCName": work["primary_location"]["raw_source_name"],
+        "Rating": "/", # TBD
+        "Year": work["publication_year"],
+        "Month": 1, # TBD
+        "Citation": work["cited_by_count"],
+        "WithAdvisor": with_advisor,
+        "Star": "No", # TBD
+        "Order": 2, # TBD
+        "AuthorCount": len(work["authorships"]),
+        "Authors": author_list
+    }
+    Works.insert(0,publication)
 
 result = {
     "Name": author["display_name"],
     "Age": "30.0", # guess
     "Research": [ x["display_name"] for x in author["topics"] ],
-    "Mobility": Mobility
+    "Mobility": Mobility,
+    "Publication": Works
 }
 
-print(result)
+print(json.dumps(result,indent=2))
 #"Mobility": [
